f6120d01daac3abff526421cb9b9e7bad3adec3b
jmjles <jmjles@gmail.com>
2023-06-21 16:33:03 -0700

    devop: added cta

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import island from "../assets/island.jpeg";
 import Image from "next/image";
 import Quotes from "../Quotes";
+import Link from "next/link";
 
 const Banner = (props: props) => {
   return (
@@ -16,6 +17,26 @@ const Banner = (props: props) => {
         fill={true}
       />
       <h1 style={{ zIndex: 10 }}>{props.heading}</h1>
+      {props.home && (
+        <Link
+          href="/locations"
+          style={{
+            position: "absolute",
+            right: 32,
+            bottom: "50%",
+            transform: "translateY(50%)",
+            backgroundColor: "#F1EFE4",
+            paddingLeft: 18,
+            paddingRight: 18,
+            borderRadius: 10,
+            cursor: "pointer",
+            color: "black",
+            textDecorationLine: "none",
+          }}
+        >
+          <p style={{}}>Explore What Our Island Has To Offer</p>
+        </Link>
+      )}
       {props.home && (
         <section className="Summary">
           Discover Taniti, a tropical paradise in the Pacific with diverse
